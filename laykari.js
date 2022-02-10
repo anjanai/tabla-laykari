27fc7dbc93aeaee91a47a3e81aa1a3b5e356f738
Anjana Iyer <anjanai.yahoo@gmail.com>
2022-02-10 12:31:30 -0800

    Add gaps as a parameter

--- laykari.js
+++ laykari.js
@@ -14,15 +14,18 @@ if (!String.prototype.format) {
 $( document ).ready(function() {
     const urlParams = new URLSearchParams(window.location.search);
     const n = urlParams.get('n');
+    var gaps = urlParams.get('gaps');
+    if (gaps === "") gaps = 1;
+	
 
-    for (let i=1; i<16; i++) {
+    for (let i=1; i<=16; i++) {
 	//console.log ("i = {0}".format(i));
 	let str = "";
 	for (let j=1; j<=n; j++) {
 	    let jstr = j.toString();
 	    str += jstr;
 	    for (let k=1; k<i; k++)
-		str += k%2 ? "-" : jstr;
+		str += gaps & k%2 ? "-" : jstr;
 	}
 
 	let arr = str.match(new RegExp('.{1,' + n + '}', 'g'));
